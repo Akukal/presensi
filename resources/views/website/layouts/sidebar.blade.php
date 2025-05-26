0858d22840249694bc26c30515356c0f9ecfe7e2
Your Name <you@example.com>
2025-05-26 10:30:23 +0700

    desain

--- resources/views/website/layouts/sidebar.blade.php
+++ resources/views/website/layouts/sidebar.blade.php
@@ -141,12 +141,6 @@
           </ul>
         </li>
         @endcanany
-        {{-- <li class="nav-item">
-          <a href="/" class="nav-link">
-            <i class="nav-icon fas fa-comment"></i>
-            <p>WhatsApp</p>
-          </a>
-        </li> --}}
 
         <!-- RFID -->
         @can(['view rfid', 'delete rfid'])
